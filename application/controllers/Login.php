f3c231ef7aee836b4fc18d282392066d91f228cc
Jeffrey Lam <jeffreylam2008@gmail.com>
2019-01-22 01:07:53 +0800

    20901220107

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -38,7 +38,6 @@ class Login extends CI_Controller
 	}
 	public function dologin()
 	{
-		
 		$_api_body = [];
 		// Get user input here
 		$_rememberme = $this->input->post("i-rememberme");
@@ -46,12 +45,35 @@ class Login extends CI_Controller
 		$_api_body["password"] = $this->input->post('i-password',true);
 		$_api_body["shopcode"] = $this->input->post('i-shops',true);
 		$_api_body = json_encode($_api_body, true);
-		echo $_api_body;
-		echo "<br>";
+		// echo $_api_body;
+		// echo "<br>";
 		$this->component_api->SetConfig("body", $_api_body);
 		$this->component_api->SetConfig("url", $this->config->item('api_url')."/systems/login/");
 		$this->component_api->CallPost();
 		$_result = json_decode($this->component_api->GetConfig("result"), true);
 		var_dump($_result);
+		// has token return from API
+		if(!empty($_result['query']))
+		{
+			$_profile = [];
+			$_profile['token'] = $_result['query'];
+			
+			$_profile['profile'] = [
+				'username' => $this->input->post('i-username',true),
+				'password' => $this->input->post('i-password',true),
+				'shopcode' => $this->input->post('i-shops',true)
+			];
+			if($_rememberme)
+			{
+				//$this->session->set_userdata('profile',$_profile['profile']);
+			}
+			else
+			{
+				//$this->session->mark_as_temp($_profile['profile'], 3600);
+			}
+			
+			var_dump($_profile);
+		}
+		
 	}
 }
