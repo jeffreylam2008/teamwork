7afd91d3b570402c151fa430c9c3f4a74b5a1adf
Jeffrey Lam <jeffreylam2008@gmail.com>
2019-02-09 00:24:17 +0800

    201902091224

--- application/controllers/Dushboard.php
+++ application/controllers/Dushboard.php
@@ -12,58 +12,61 @@ class Dushboard extends CI_Controller
 
 		$_username = "iamadmin";
 
-		var_dump($_SESSION);
+		//var_dump($_SESSION);
 		$_profile = $this->session->userdata('profile');
 		$_token = $_profile['token'];
 
 		if(!empty($_token))
 		{
+			// check validation token in Server side
 			$this->component_api->SetConfig("url", $this->config->item('api_url')."/systems/login/".$_token);
 			$this->component_api->CallGet();
-			$_employee = json_decode($this->component_api->GetConfig("result"),true);
-
-
-			// sidebar session
-			$_param = $this->router->fetch_class()."/".$this->router->fetch_method();
-			// $this->session->sess_destroy();
-			// unset($_SESSION);
-			
-
-		
-			
-			// check token API
-			
-			
-			$this->_inv_header_param["topNav"] = [
-				"isLogin" => true,
-				"username" => $_username,
-				"employee_code" => "110022",
-				"shop_code" => "0012",
-				"today" => date("Y-m-d")
-			];
-			// fatch side bar API
-			$this->component_api->SetConfig("url", $this->config->item('api_url')."/systems/menu/side");
-			$this->component_api->CallGet();
-			$_nav_list = json_decode($this->component_api->GetConfig("result"), true);
-			$this->component_sidemenu->SetConfig("nav_list", $_nav_list);
-			$this->component_sidemenu->SetConfig("active", $_param);
-			$this->component_sidemenu->Proccess();
+			$_api_result = json_decode($this->component_api->GetConfig("result"),true);
 			// echo "<pre>";
-			// var_dump( $this->component_sidemenu->GetConfig("slug"));
+			// var_dump($_api_result);
 			// echo "</pre>";
-			
-			// load header view
-			$this->load->view('header',[
-				'title'=>'Dushboard',
-				'sideNav_view' => $this->load->view('side-nav', [
-					"sideNav"=>$this->component_sidemenu->GetConfig("nav_finished_list"),
-					"path"=>$this->component_sidemenu->GetConfig("path"),
-					"param"=> $_param
-				], TRUE), 
-				'topNav_view' => $this->load->view('top-nav', [
-					"topNav" => $this->_inv_header_param["topNav"]
-				], TRUE)
-			]);
+			if(!empty($_api_result['query']))
+			{
+				// sidebar session
+				$_param = $this->router->fetch_class()."/".$this->router->fetch_method();
+				// $this->session->sess_destroy();
+				// unset($_SESSION);
+				// check token API
+				$this->_inv_header_param["topNav"] = [
+					"isLogin" => true,
+					"username" => $_username,
+					"employee_code" => "110022",
+					"shop_code" => "0012",
+					"today" => date("Y-m-d")
+				];
+				// fatch side bar API
+				$this->component_api->SetConfig("url", $this->config->item('api_url')."/systems/menu/side");
+				$this->component_api->CallGet();
+				$_nav_list = json_decode($this->component_api->GetConfig("result"), true);
+				$this->component_sidemenu->SetConfig("nav_list", $_nav_list);
+				$this->component_sidemenu->SetConfig("active", $_param);
+				$this->component_sidemenu->Proccess();
+				// echo "<pre>";
+				// var_dump( $this->component_sidemenu->GetConfig("slug"));
+				// echo "</pre>";
+				
+				// load header view
+				$this->load->view('header',[
+					'title'=>'Dushboard',
+					'sideNav_view' => $this->load->view('side-nav', [
+						"sideNav"=>$this->component_sidemenu->GetConfig("nav_finished_list"),
+						"path"=>$this->component_sidemenu->GetConfig("path"),
+						"param"=> $_param
+					], TRUE), 
+					'topNav_view' => $this->load->view('top-nav', [
+						"topNav" => $this->_inv_header_param["topNav"]
+					], TRUE)
+				]);
+			}
+			else
+			{
+				redirect(base_url("login?url=".urlencode(base_url("dushboard"))),"refresh");
+			}
 		}
 		else
 		{
