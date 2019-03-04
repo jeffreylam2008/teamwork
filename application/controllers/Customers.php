669f34911fc8fd03f45ca11320150a2245316a16
jeffreylam2008 <jeffreylam2008@gmail.com>
2019-03-04 20:33:43 +0800

    201903042033

--- application/controllers/Customers.php
+++ application/controllers/Customers.php
@@ -3,52 +3,75 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Customers extends CI_Controller 
 {
+	var $_inv_header_param = [];
 	public function __construct()
 	{
 		parent::__construct();
 		
 		// dummy data
 		//	var_dump(array_keys($_SESSION));
-		
-		
-		$username = "iamadmin";
-		// read URI
-		$_param = $this->router->fetch_class()."/".$this->router->fetch_method();
-		
-		// fatch employee API
-		$this->component_api->SetConfig("url", $this->config->item('api_url')."/systems/employee/".$username);
-		$this->component_api->CallGet();
-		$_employee = json_decode($this->component_api->GetConfig("result"),true);
-		//var_dump($_employee);
-		$this->_inv_header_param["topNav"] = [
-			"isLogin" => true,
-			"username" => $username,
-			"employee_code" => "110022",
-			"shop_code" => "0012",
-			"today" => date("Y-m-d")
-		];
-		// Call API here
-		$this->component_api->SetConfig("url", $this->config->item('api_url')."/systems/menu/side");
-		$this->component_api->CallGet();
-		$_nav_list = json_decode($this->component_api->GetConfig("result"), true);
-		$this->component_sidemenu->SetConfig("nav_list", $_nav_list);
-		$this->component_sidemenu->SetConfig("active", $_param);
-		$this->component_sidemenu->Proccess();
 
-		// load header view
-		$this->load->view('header',[
-			'title'=>'Customers',
-			'sideNav_view' => $this->load->view('side-nav', [
-				"sideNav"=>$this->component_sidemenu->GetConfig("nav_finished_list"),
-				"path"=>$this->component_sidemenu->GetConfig("path"),
-				"param"=> $_param
-			], TRUE), 
-			'topNav_view' => $this->load->view('top-nav', [
-				"topNav" => $this->_inv_header_param["topNav"]
-			], TRUE)
-		]);
-		// load breadcrumb
-		//$this->load->view('breadcrumb');
+		// call token from session
+		$_token = $this->session->userdata['profile']['token'];
+		
+		// API call
+		$this->load->library("component_login",[$_token, "products/items"]);
+		
+		// login session
+		if(!empty($this->component_login->CheckToken()))
+		{
+			$_username = $this->session->userdata['profile']['profile']['username'];
+			// read URI
+			$_param = $this->router->fetch_class()."/".$this->router->fetch_method();
+			
+			// fatch master
+			$this->_inv_header_param["topNav"] = [];
+			foreach($_SESSION['master']['employee']['query'] as $key => $val)
+			{
+				if($val['username'] === $_username)
+				{
+
+					$this->_inv_header_param["topNav"] = [
+						"username" => $val['username'],
+						"employee_code" =>  $val['employee_code'],
+						"shop_code" => $val['default_shopcode'],
+						"role" => $val['role'],
+						"today" => date("Y-m-d")
+					];
+				}
+			}
+			
+			// debug display
+			var_dump($this->_inv_header_param["topNav"]);
+			
+			
+			// Call API here
+			$this->component_api->SetConfig("url", $this->config->item('api_url')."/systems/menu/side");
+			$this->component_api->CallGet();
+			$_nav_list = json_decode($this->component_api->GetConfig("result"), true);
+			$this->component_sidemenu->SetConfig("nav_list", $_nav_list);
+			$this->component_sidemenu->SetConfig("active", $_param);
+			$this->component_sidemenu->Proccess();
+
+			// load header view
+			$this->load->view('header',[
+				'title'=>'Customers',
+				'sideNav_view' => $this->load->view('side-nav', [
+					"sideNav"=>$this->component_sidemenu->GetConfig("nav_finished_list"),
+					"path"=>$this->component_sidemenu->GetConfig("path"),
+					"param"=> $_param
+				], TRUE), 
+				'topNav_view' => $this->load->view('top-nav', [
+					"topNav" => $this->_inv_header_param["topNav"]
+				], TRUE)
+			]);
+			// load breadcrumb
+			//$this->load->view('breadcrumb');
+		}
+		else
+		{
+			redirect(base_url("login?url=".urlencode($this->component_login->GetRedirectURL())),"refresh");
+		}
 	}
 	public function index($page=1)
 	{
@@ -110,11 +133,10 @@ class Customers extends CI_Controller
 		//$this->session->sess_destroy();
 		$_data = [];
 		
-		
 		// user data
 		$_page = $this->session->userdata("page");
 
-
+		
 		if(!empty($cust_code))
 		{
 			// Call API here
